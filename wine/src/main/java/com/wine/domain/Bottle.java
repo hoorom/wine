57cc5216f622b29720d52a3f0308fe0b1a8c25cb
Troll <Troll@Troll-PC>
2020-03-24 15:39:21 +0100

    Ajout hibernate / postgres

--- wine/src/main/java/com/wine/domain/Bottle.java
+++ wine/src/main/java/com/wine/domain/Bottle.java
@@ -1,9 +1,20 @@
 package com.wine.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity	
+@Table(name="BOTTLE")
 public class Bottle {
 	
+	@Id
+    @GeneratedValue
 	private Long id;
 
+	@Column(name="name")
 	private String name;
 
 	public Long getId() {
