b480b1fad8d3d76c117bef545385f3c2651cecd8
Troll <Troll@Troll-PC>
2020-04-17 18:33:54 +0200

    Création de compte

--- src/main/java/com/wine/auth/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.wine.auth;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
