b7a28ba4b3a41b46f8aad156c09a979b0c9e7a7d
Troll <Troll@Troll-PC>
2020-04-22 15:52:47 +0200

    Add french labels

---
+++ src/main/java/com/wine/bottle/util/WineLocation.java
@@ -0,0 +1,9 @@
+package com.wine.bottle.util;
+
+/**
+ * Region viticole
+ */
+public enum WineLocation {
+	ALSACE, OTHER, BEAUJOLAIS, BORDELAIS, BOURGOGNE, CHAMPAGNE, COGNAC, CORSE, JURA, BUGEY, SAVOIE,
+	LANGUEDOC_ROUSSILLON, PROVENCE, SUD_OUEST, LOIRE, RHONE
+}
